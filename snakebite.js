6cd85b0b4ad1055a66a5521856e4b13179f2937c
dwight <dwight@10gen.com>
2016-06-07 16:39:41 -0400

    more

--- snakebite.js
+++ snakebite.js
@@ -11,9 +11,12 @@ var playing = false;
 
 var score = 0;
 var nBalls = 6;
+var MaxBalls = 16;
 var ballDiameter = 20;
 
 var balls = document.getElementsByTagName("circle");
+var cats = document.getElementsByTagName("image");
+var nCats = cats.length;
 
 var ballPositionsX=[0,0,0,0,0,0];
 var ballPositionsY=[0,0,0,0,0,0];
@@ -24,13 +27,39 @@ var svg = document.getElementById('svgRoot');
 
 var playground = svg.getBoundingClientRect();
 
-function gameOver() {
+function gameOver(msg) {
     playing = false;
-    info.innerHTML = "Game Over!  Press space to play again.";
+    info.innerHTML = "Game Over! " + msg + "Press space to play again.";
     endSound.play();
 }
 
 var svgns = "http://www.w3.org/2000/svg";
+
+/*
+function weOverlap(r1, r2) { 
+    return !(r2.left > r1.right || 
+           r2.right < r1.left || 
+           r2.top > r1.bottom ||
+           r2.bottom < r1.top);
+}
+*/
+
+// are we hitting something?  this isn't very fancy just seeing if we are 
+// in roughly the same position, by looking at the centers.
+//
+// when we do left+right, we are really trying to find the middle, or average, 
+// (left+right) divided by two.  since we have this for both r1 and r2, we don't 
+// bother doing the division, although it makes the number on the far right bigger.
+//
+function weOverlap(r1, r2) { 
+    if (
+        Math.abs(r1.left+r1.right)-(r2.left+r2.right) <= 20 &&
+        Math.abs(r1.top+r1.bottom)-(r2.top+r2.bottom) <= 20  
+        ) {
+        return true;
+    }
+    return false;
+}
 
 function checkBounds() { 
     var ourPos = balls[0].getBoundingClientRect();
@@ -41,9 +70,30 @@ function checkBounds() {
         ourPos.right > playground.right
     ) { 
         // out of playground
-        gameOver();
+        gameOver("");
         return false;
     }
+
+    // make sure we didn't hit ourself
+    for( i = 1; i < nBalls; i++ ) { 
+        if( ballPositionsX[i] == ballPositionsX[0] && ballPositionsY[i] == ballPositionsY[0] ) { 
+            gameOver("You hit yourself. ");
+            return false;
+        }
+    }
+
+    // hit cat?
+    console.log(nCats);
+    for( i = 0; i < nCats; i++ ) { 
+        var catRect = cats[i].getBoundingClientRect();
+        console.log(catRect);
+    console.log(ourPos);
+        if( weOverlap(ourPos, catRect) ) { 
+            gameOver("Drats. ");
+            return false;
+        }
+    }
+
     return true;
 }
 
@@ -128,7 +178,25 @@ var endSound = new Howl({
 // we will run myTimer function ever so many milliseconds to do things
 setInterval(myTimer, 1000);
 
+var z = 0;
+var interval = 4;
+
+function addBall() { 
+    if( nBalls < MaxBalls ) {
+        z = 0;
+        interval++;
+        //ballsPositionX[nBalls] = ballsPositionX[nBalls-1];
+        //ballsPositionY[nBalls] = ballsPositionY[nBalls-1];
+        nBalls++;
+    }
+}
+
 function move() { 
+    if( ++z % interval == 0 ) { 
+        addBall();
+        //score += 10;
+    }
+
     for( i = nBalls-1; i >= 1; i--) {
         // move body segment forward to position of the ahead segment
         // on the last round
